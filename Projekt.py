2868249b960ac4babd22a0c052a468a63d2935d7
nguyenmy256 <mytrinhnguyen@mys-imac.speedport_w_723v_1_39_000>
2015-12-31 17:48:54 +0100

    7th Commit
    Formale Veränderungen

--- Projekt.py
+++ Projekt.py
@@ -42,17 +42,17 @@ def quadrat():
         x= input("Bitte gib an ob du den Flächeninhalt[f], den Umfang[u] oder beides[b] vom Quadrat ausrechnen willst. ")
         
         if x=="f":
-            a=float(input("Gib die Länge des Quadrates an. "))
-            quadrata= a*a
+            a=float(input("Gebe bitte die Länge des Quadrates an: "))
+            quadrata= a**2
             print ("Der Flächeninhalt des Quadrats beträgt",quadrata,"Flächeneinheiten.")
         elif x=="u":
-            a=float(input("Gib die Länge des Quadrates an. "))
+            a=float(input("Gebe bitte die Länge des Quadrates an: "))
             quadratu= 4*a
             print ("Der Umfang des Quadrates beträgt", quadratu,"Flächeneinheiten.")
         else:
-            a=float(input("Gib die Länge des Quadrates an. "))
+            a=float(input("Gebe bitte die Länge des Quadrates an. "))
             quadratu= 4*a
-            quadrata= a*a
+            quadrata= a**2
             print ("Der Umfang des Quadrates beträgt", quadratu,"Flächeneinheiten.")
             print ("Der Flächeninhalt des Quadrats beträgt",quadrata,"Flächeneinheiten.")
 
@@ -63,18 +63,18 @@ def rechteck():
              print ("Sorry, aber das habe ich nicht verstanden.")
         x= input("Bitte gib an ob du den Flächeninhalt[f], den Umfang[u] oder beides[b] vom Rechteck ausrechnen willst. ")
         if x=="f":
-            a= float(input("Gib die Länge des Rechtecks an. "))
-            b= float(input("Gib die Breite des Rechtecks an. "))
+            a= float(input("Gebe bitte die Länge des Rechtecks an: "))
+            b= float(input("Gebe bitte die Breite des Rechtecks an: "))
             rechtecka= a*b
             print("Der Flächeninhalt des Rechtecks beträgt", rechtecka, "Flächeneinheiten.")
         elif x=="u":
-            a= float(input("Gib die Länge des Rechtecks an. "))
-            b= float(input("Gib die Breite des Rechtecks an. "))
+            a= float(input("Gebe bitte die Länge des Rechtecks an: "))
+            b= float(input("Gebe bitte die Breite des Rechtecks an: "))
             rechtecku= 2*a+2*b
             print("Der Umfang des Rechtecks beträgt", rechtecku, "Flächeneinheiten.")
         else:
-            a= float(input("Gib die Länge des Rechtecks an. "))
-            b= float(input("Gib die Breite des Rechtecks an. "))
+            a= float(input("Gebe bitte die Länge des Rechtecks an: "))
+            b= float(input("Gebe bitte die Breite des Rechtecks an: "))
             rechtecka= a*b
             rechtecku= 2*a+2*b
             print("Der Flächeninhalt des Rechtecks beträgt", rechtecka, "Flächeneinheiten.")
@@ -87,22 +87,22 @@ def dreieck():
              print ("Sorry, aber das habe ich nicht verstanden.")
         x= input("Bitte gib an ob du den Flächeninhalt[f], den Umfang[u] oder beides[b] vom Dreieck ausrechnen willst. ")
         if x=="f":
-            g= float(input("Gib die Länge der Basis oder der Grundseite des Dreiecks an. "))
-            h= float(input("Gib die Höhe des Dreiecks an. "))
+            g= float(input("Gebe bitte die Länge der Basis oder der Grundseite des Dreiecks an: "))
+            h= float(input("Gebe bitte die Höhe des Dreiecks an: "))
             dreiecka= (g*h)/2
             print("Der Flächeninhalt des Dreiecks beträgt", dreiecka, "Flächeneinheiten.")
         elif x=="u":
-            a= float(input("Gib die Länge der 1. Seite des Dreiecks an. "))
-            b= float(input("Gib die Länge der 2. Seite des Dreiecks an. "))
-            c= float(input("Gib die Länge der 3. Seite des Dreiecks an. "))
+            a= float(input("Gebe bitte die Länge der 1. Seite (Kathete) des Dreiecks an: "))
+            b= float(input("Gebe bitte die Länge der 2. Seite (Kathete) des Dreiecks an: "))
+            c= float(input("Gebe bitte die Länge der 3. Seite (Hypotenuse) des Dreiecks an: "))
             dreiecku= a+b+c
             print("Der Umfang des Dreiecks beträgt", dreiecku, "Flächeneinheiten.")
         else:
-            g= float(input("Gib die Länge der Basis oder der Grundseite des Dreiecks an. "))
-            h= float(input("Gib die Höhe des Dreiecks an. "))
+            g= float(input("Gebe bitte die Länge der Basis oder der Grundseite des Dreiecks an: "))
+            h= float(input("Gebe bitte die Höhe des Dreiecks an: "))
             dreiecka= (g*h)/2
-            a= float(input("Gib die Länge des 1. Schenkel des Dreiecks an. "))
-            b= float(input("Gib die Länge der 2. Schenkel des Dreiecks an. "))
+            a= float(input("Gebe bitte die Länge des 1. Schenkels des Dreiecks an: "))
+            b= float(input("Gebe bitte die Länge des 2. Schenkels des Dreiecks an: "))
             dreiecku= a+b+g
             print("Der Flächeninhalt des Dreiecks beträgt", dreiecka, "Flächeneinheiten.")
             print("Der Umfang des Dreiecks beträgt", dreiecku, "Flächeneinheiten.")
@@ -113,23 +113,22 @@ def parallelogramm():
         if x is not None:
             print ("Sorry, aber das habe ich nicht verstanden.")
         x= input("Bitte gib an ob du den Flächeninhalt[f], den Umfang[u] oder beides[b] vom Parallelogramm ausrechnen willst. ")
-    
         if x=="f":
-            g= float(input("Gib die Länge der Grundseite des Parallelograms an. "))
-            h= float(input("Gib die Höhe des Parallelograms an. "))
+            g= float(input("Gebe bitte die Länge der Grundseite des Parallelograms an: "))
+            h= float(input("Gebe bitte die Höhe des Parallelograms an: "))
             parallelogramma= g*h
             print ("Der Flächeninhalt des Paralellograms beträgt",parallelogramma,"Flächeneinheiten. ")
         elif x=="u":
-            a= float(input("Gib die Länge der 1. Seite des Parallelogramms an"))
-            b= float(input("Gib die Länge der 2. Seite des Paralellogramms an"))
+            a= float(input("Gebe bitte die Länge der 1. Seite des Parallelogramms an: "))
+            b= float(input("Gebe bitte die Länge der 2. Seite des Paralellogramms an: "))
             parallelogrammu= 2*a+2*b
             print ("Der Umfang des Parallelogramms beträgt", parallelogrammu,"Flächeneinheiten. ")
         else:
-            g= float(input("Gib die Länge der Grundseite des Parallelograms an. "))
-            h= float(input("Gib die Höhe des Parallelograms an. "))
+            g= float(input("Gebe bitte die Länge der Grundseite des Parallelograms an: "))
+            h= float(input("Gebe bitte die Höhe des Parallelograms an: "))
             parallelogramma= g*h
-            a= float(input("Gib die Länge der 1. Seite des Parallelogramms an. "))
-            b=float(input("Gib die Länge der 2. Seite des Parallelogramms an. "))
+            a= float(input("Gene bitte die Länge der 1. Seite des Parallelogramms an: "))
+            b=float(input("Gebe bitte die Länge der 2. Seite des Parallelogramms an: "))
             parallelogrammu= 2*a+2*b
             print ("Der Flächeninhalt des Paralellograms beträgt",parallelogramma,"Flächeneinheiten.")        
             print ("Der Umfang des Parallelogramms beträgt", parallelogrammu,"Flächeneinheiten.")
@@ -141,62 +140,62 @@ def trapez():
             print ("Sorry, aber das habe ich nicht verstanden.")
         x= input ("Bitte gib an ob du den Flächeninhalt[f], den Umfang[u] oder beides[b] vom Trapez ausrechnen willst. ")
         if x=="f":
-            a=float(input("Gib die Länge der 1. parallelen Seite des Trapez' an. "))
-            c=float(input("Gib die Länge der 2. parallelen Seite des Trapez' an. "))
-            h=float(input("Gib die Höhe des Trapez' an. "))
+            a=float(input("Gebe bitte die Länge der 1. parallelen Seite des Trapez' an: "))
+            c=float(input("Gebe bitte die Länge der 2. parallelen Seite des Trapez' an: "))
+            h=float(input("Gebe bitte die Höhe des Trapez' an: "))
             trapeza= ((a+c)/2)*h
             print ("Der Flächeninhalt des Trapez' beträgt",trapeza,"Flächeneinheiten.")
         elif x=="u":
-            a=float(input("Gib die Länge der 1. Seite des Trapez' an"))
-            b=float(input("Gib die Länge der 2. Seite des Trapez' an. "))
-            c=float(input("Gib die Länge der 3. Seite des Trapez' an. "))
-            d=float(input("Gib die Länge der 1. Seite des Trapez' an. "))
+            a=float(input("Gebe bitte die Länge der 1. Seite des Trapez' an: "))
+            b=float(input("Gebe bitte die Länge der 2. Seite des Trapez' an: "))
+            c=float(input("Gebe bitte die Länge der 3. Seite des Trapez' an: "))
+            d=float(input("Gebe bitte die Länge der 1. Seite des Trapez' an: "))
             trapezu=a+b+c+d
             print ("Der Umfang des Trapez' beträgt",trapezu,"Flächeneinheiten.")
         else:
-            a=float(input("Gib die Länge der 1. parallelen Seite des Trapez' an. "))
-            c=float(input("Gib die Länge der 2. parallelen Seite des Trapez' an. "))
-            h=float(input("Gib die Höhe des Trapez' an. "))
+            a=float(input("Gebe bitte die Länge der 1. parallelen Seite des Trapez' an: "))
+            c=float(input("Gebe bitte die Länge der 2. parallelen Seite des Trapez' an: "))
+            h=float(input("Gebe bitte die Höhe des Trapez' an: "))
             trapeza= ((a+c)/2)*h
-            a=float(input("Gib die Länge der 1. Seite des Trapez' an. "))
-            b=float(input("Gib die Länge der 2. Seite des Trapez' an. "))
-            c=float(input("Gib die Länge der 3. Seite des Trapez' an. "))
-            d=float(input("Gib die Länge der 1. Seite des Trapez' an. "))
+            a=float(input("Gebe bitte die Länge der 1. Seite des Trapez' an: "))
+            b=float(input("Gebe bitte die Länge der 2. Seite des Trapez' an: "))
+            c=float(input("Gebe bitte die Länge der 3. Seite des Trapez' an: "))
+            d=float(input("Gebe bitte die Länge der 1. Seite des Trapez' an: "))
             trapezu=a+b+c+d
             print ("Der Flächeninhalt des Trapez' beträgt",trapeza,"Flächeneinheiten.")
             print ("Der Umfang des Trapez' beträgt",trapezu,"Flächeneinheiten.")
 
 def kreis():
     x= None
-    while x not in ("f", "b","u"):
+    while x not in ("f", "b","u", "a"):
         if x is not None:
             print ("Sorry, aber das habe ich nicht verstanden.")
         x= input ("Bitte gib an ob du den Flächeninhalt[f], den Umfang[u], den Durchmesser[d] oder alles[a] vom Kreis ausrechnen willst. ")
         if x=="f":
             y=input("Ist der Radius[r] oder der Durchmesser[d] vorhanden?")
             if y=="r":
-                r=float(input("Gib den Radius vom Kreis an. "))
-                kreisa= math.pi*(r*r)
+                r=float(input("Gebe bitte den Radius vom Kreis an: "))
+                kreisa= math.pi*r**2
             else:
-                d=float(input("Gib den Durchmesser vom Kreis an "))
-                kreisa= math.pi*((d*d)/4)
+                d=float(input("Gebe bitte den Durchmesser vom Kreis an: "))
+                kreisa= math.pi*((d/2)**2)
             print ("Der Flächeninhalt vom Kreis beträgt", kreisa, "Flächeneinheiten.")
         elif x=="u":
             y=input("Ist der Radius[r] oder der Durchmesser[d] vorhanden?")
             if y=="r":
-                r=float(input("Gib den Radius vom Kreis an. "))
+                r=float(input("Gebe bitte den Radius vom Kreis an: "))
                 kreisu= 2*math.pi*r
             else:
-                d=float(input("Gib den Durchmesser vom Kreis an. "))
+                d=float(input("Gebe bitte den Durchmesser vom Kreis an: "))
                 kreisa= math.pi*d
             print ("Der Umfang vom Kreis beträgt", kreisu, "Flächeneinheiten.")
         elif x=="d":
-                r=float(input("Gib den Radius vom Kreis an."))
+                r=float(input("Gebe bitte den Radius vom Kreis an: "))
                 kreisd= r*2
                 print ("Der Durchmesser beträgt", kreisd,"Flächeneinheiten.")
         else:
-                r=float(input("Gib den Radius vom Kreis an. "))
-                kreisa=math.pi*(r*r)
+                r=float(input("Gebe bitte den Radius vom Kreis an: "))
+                kreisa=math.pi*r**2
                 kreisu=2*math.pi*r
                 kreisd=r*2
                 print ("Der Flächeninhalt vom Kreis beträgt", kreisa, "Flächeneinheiten.")
@@ -205,25 +204,25 @@ def kreis():
         
 def kreisbogen_kreissektor():
     x= None
-    while x not in ("ks", "kb","u"):
+    while x not in ("ks", "kb","b"):
         if x is not None:
             print ("Sorry, aber das habe ich nicht verstanden.")
         x=input ("Bitte gib an ob du den Kreissektor[ks], Kreisbogen[kb] oder beides[b] berechnen willst. ")
         if x=="ks":
-            r=float(input("Gib den Radius des Kreises an. "))
-            α=float(input("Gib den Winkel α des Kreissektors an. "))
-            ks=((math.pi*(r*r)*math.radians(α)))/math.radians(360)
+            r=float(input("Gebe bitte den Radius des Kreises an: "))
+            α=float(input("Gebe bitte den Winkel α des Kreissektors an: "))
+            ks=math.pi*r**2*(math.radians(α)/math.radians(360))
             print ("Der Kreissektor beträgt",ks,"Flächeneinheiten.")
         elif x=="kb":
-            r=float(input("Gib den Radius des Kreises an. "))
-            α=float(input("Gib den Winkel α des Kreissektors an. "))
-            kb=(math.pi*r*math.radians(α))/math.radians (180)
+            r=float(input("Gebe bitte den Radius des Kreises an: "))
+            α=float(input("Gebe bitte den Winkel α des Kreissektors an: "))
+            kb=math.pi*r*(math.radians(α)/math.radians (180))
             print ("Der Kreisbogen beträgt",kb,"Flächeneinheiten.")
         else:
-            r=float(input("Gib den Radius des Kreises an. "))
-            α=float(input("Gib den Winkel α des Kreissektors an. "))
-            ks=((math.pi*(r*r)*math.radians(α)))/math.radians(360)
-            kb=(math.pi*r*math.radians(α))/math.radians (180)
+            r=float(input("Gebe bitte den Radius des Kreises an: "))
+            α=float(input("Gebe bitte den Winkel α des Kreissektors an: "))
+            ks=math.pi*r**2(*math.radians(α)/math.radians(360))
+            kb=math.pi*r*(math.radians(α)/math.radians (180))
             print ("Der Kreissektor beträgt",ks,"Flächeneinheiten.")        
             print ("Der Kreisbogen beträgt",kb,"Flächeneinheiten.")
         
@@ -234,17 +233,17 @@ def würfel():
             print ("Sorry, aber das habe ich nicht verstanden.")
         x=input("Möchtest du das Volumen[v], die Oberfläche[o] oder beides[b] vom Würfel berechnen? ")
         if x=="v":
-            a=float(input("Gib die Länge einer Seite des Würfels an. "))
+            a=float(input("Gebe bitte die Kantenlänge des Würfels an: "))
             würfelv=(a*a*a)
             print ("Das Volumen des Würfels beträgt",würfelv,"Flächeneinheiten.")
         elif x=="o":
-            a=float(input("Gib die Länge einer Seite des Würfels an. "))
-            würfelo= (6*(a*a))
+            a=float(input("Gebe bitte die Kantenlänge des Würfels an: "))
+            würfelo= 6*a**2
             print ("Die Oberfläche des Würfels beträgt",würfelo,"Flächeneinheiten.")
         elif x=="b":
-            a=float(input("Gib die Länge einer Seite des Würfels an. "))
+            a=float(input("Gebe bitte die Kantenlänge des Würfels an: "))
             würfelv=(a*a*a)
-            würfelo= (6*(a*a))
+            würfelo= 6*a**2
             print ("Das Volumen des Würfels beträgt",würfelv,"Flächeneinheiten.")
             print ("Die Oberfläche des Würfels beträgt",würfelo,"Flächeneinheiten.")
 
@@ -255,21 +254,21 @@ def quader():
             print ("Sorry, aber das habe ich nicht verstanden.")
         x=input ("Möchtest du das Volumen[v], die Oberfläche[o] oder beides[b] des Quaders berechnen? ")
         if x=="v":
-            a= float(input("Gib die Länge des Quaders an. "))
-            b= float(input("Gib die Breite des Quaders an. "))
-            c= float (input("Gib die Höhe des Quaders an. "))
+            a= float(input("Gebe bitte die Länge des Quaders an: "))
+            b= float(input("Gebe bitte die Breite des Quaders an: "))
+            c= float (input("Gebe bitte die Höhe des Quaders an: "))
             quaderv= a*b*c
             print ("Das Volumen des Quaders beträgt",quaderv,"Flächeneinheiten.")
         elif x=="o":
-            a= float(input("Gib die Länge des Quaders an. "))
-            b= float(input("Gib die Breite des Quaders an. "))
-            c= float (input("Gib die Höhe des Quaders an. "))
+            a= float(input("Gebe bitte die Länge des Quaders an: "))
+            b= float(input("Gebe bitte die Breite des Quaders an: "))
+            c= float (input("Gebe bitte die Höhe des Quaders an: "))
             quadero= 2*a*b+2*a*c+2*b*c
             print ("Die Oberfläche des Quaders beträgt",quadero,"Flächeneinheiten.")
         elif x=="b":
-            a= float(input("Gib die Länge des Quaders an. "))
-            b= float(input("Gib die Breite des Quaders an. "))
-            c= float (input("Gib die Höhe des Quaders an. "))
+            a= float(input("Gebe bitte die Länge des Quaders an: "))
+            b= float(input("Gebe bitte die Breite des Quaders an: "))
+            c= float (input("Gebe bitte die Höhe des Quaders an: "))
             quadero= 2*a*b+2*a*c+2*b*c
             quaderv= a*b*c
             print ("Das Volumen des Quaders beträgt",quaderv,"Flächeneinheiten.")
@@ -282,24 +281,24 @@ def prisma():
             print ("Sorry, aber das habe ich nicht verstanden.")
         x=input("Soll das Volumen[v], die Oberfläche[o], die Mantelfläche [m] oder alles [a] vom Prisma berechnet werden? ")
         if x=="v":
-            g= float(input("Gib die Grundfläche des Prismas an. "))
-            h=float(input ("Gib die Höhe des Prismas an. "))
+            g= float(input("Gebe bitte die Grundfläche des Prismas an: "))
+            h=float(input ("Gebe bitte die Höhe des Prismas an: "))
             prismav= g*h
             print ("Das Volumen des Prismas beträgt", prismav,"Flächeneinheiten.")
         elif x=="o":
-            g= float (input("Gib die Grundfläche des Prismas an. "))
-            m= float (input("Gib die Mantelfläche des Prismas an. "))
+            g= float (input("Gebe bitte die Grundfläche des Prismas an: "))
+            m= float (input("Gebe die Mantelfläche des Prismas an: "))
             prismao= 2*g+m
             print ("Die Oberfläche des Prismas beträgt", prismao,"Flächeneinheiten.")
         elif x=="m":
-            h=float(input ("Gib die Höhe des Prismas an. "))
-            u=float(input("Gib den Umfang der Grundfläche des Prismas an. "))
+            h=float(input ("Gebe bitte die Höhe des Prismas an: "))
+            u=float(input("Gebe bitte den Umfang der Grundfläche des Prismas an: "))
             prismam= h*u
             print ("Die Mantelfläche des Prismas beträgt", prismam,"Flächeneinheiten.")
         else:
-            g= float(input("Gib die Grundfläche des Prismas an. "))
-            h=float(input ("Gib die Höhe des Prismas an. "))
-            u=float(input("Gib den Umfang der Grundfläche des Prismas an. "))
+            g= float(input("Gebe bitte die Grundfläche des Prismas an: "))
+            h=float(input ("Gebe bitte die Höhe des Prismas an: "))
+            u=float(input("Gebe bitte den Umfang der Grundfläche des Prismas an: "))
             prismav= g*h
             prismao= 2*g+m
             prismam= h*u
